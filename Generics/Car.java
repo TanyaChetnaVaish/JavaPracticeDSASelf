2c229cb5c4a0d91246f5ed4527d8529051bd3f4e
Tanya Chetna Vaish <63603952+TanyaChetnaVaish@users.noreply.github.com>
2021-05-02 12:55:19 +0530

    Create Car.java

---
+++ Generics/Car.java
@@ -0,0 +1,21 @@
+
+
+public class Car {
+	// implements Comparable<Car> {
+	int speed;
+	int price;
+	String color;
+	public Car(int speed,int price,String color) {
+		this.speed=speed;
+		this.price=price;
+		this.color=color;
+	}
+
+	public String toString() {
+		return "S:"+this.speed+"P:"+this.price+"C:"+this.color;
+	}
+
+
+
+
+}
