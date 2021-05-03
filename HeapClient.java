a0641406c57af480c856bd09363ecfd15eef5fb2
Tanya Chetna Vaish <63603952+TanyaChetnaVaish@users.noreply.github.com>
2021-05-03 13:48:02 +0530

    Heap Client

---
+++ HeapClient.java
@@ -0,0 +1,21 @@
+package Practice;
+
+public class HeapClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Heap heap=new Heap();
+		heap.add(10);
+		heap.display();
+		heap.add(20);
+		heap.display();
+		heap.add(30);
+		heap.display();
+		heap.add(40);
+		heap.display();
+		heap.add(5);
+		heap.display();
+		
+	}
+
+}
