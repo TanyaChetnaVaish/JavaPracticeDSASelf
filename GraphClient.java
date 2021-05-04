73b808533d9b4600298bcbafd7f496fc1ee95706
Tanya Chetna Vaish <63603952+TanyaChetnaVaish@users.noreply.github.com>
2021-05-05 00:45:40 +0530

    Create GraphClient.java

---
+++ GraphClient.java
@@ -0,0 +1,39 @@
+package Practice;
+
+public class GraphClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Graph graph=new Graph();
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
+graph.addVertex("G");
+graph.addEdge("A", "B", 2);
+graph.addEdge("A", "D", 3);
+graph.addEdge("B", "C", 1);
+graph.addEdge("C", "D", 8);
+graph.addEdge("D", "E", 10);
+graph.addEdge("E", "F", 45);
+graph.addEdge("E", "G", 7);
+graph.addEdge("F", "G", 18);
+graph.display();
+System.out.println(graph.numVertex());
+System.out.println(graph.numEdges());
+System.out.println(graph.containsEdge("A", "C"));
+graph.removeEdge("A", "B");
+graph.display();
+graph.removeVertex("F");
+graph.display();
+graph.addVertex("F");
+graph.display();
+graph.addEdge("F", "A", 11);
+graph.display();
+
+
+	}
+
+}
