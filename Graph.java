43c8889ffd82e3a1f761f0bae1fcf15225a5018d
Tanya Chetna Vaish <63603952+TanyaChetnaVaish@users.noreply.github.com>
2021-05-06 00:09:16 +0530

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -25,79 +25,559 @@ public void addVertex(String vname) {
 	vtces.put(vname, vtx);
 }
 public void removeVertex(String vname) {
-	Vertex vtx=vtces.get(vname);
-	ArrayList<String> keys=new ArrayList<>(vtx.nbrs.keySet());
-	for(String key:keys) {
-		Vertex nbrVtx=vtces.get(key);
+
+	Vertex vtx = vtces.get(vname);
+	ArrayList<String> keys = new ArrayList<>(vtx.nbrs.keySet());
+
+	for (String key : keys) {
+
+		Vertex nbrVtx = vtces.get(key);
 		nbrVtx.nbrs.remove(vname);
 	}
+
 	vtces.remove(vname);
-	
+
 }
-public void removeEdge(String vname1,String vname2) {
-	Vertex v1=vtces.get(vname1);
-	Vertex v2=vtces.get(vname2);
-	if(v1==null||v2==null||v1.nbrs.containsKey(vname2)) {
+public void removeEdge(String vname1, String vname2) {
+
+	Vertex vtx1 = vtces.get(vname1);
+	Vertex vtx2 = vtces.get(vname2);
+
+	if (vtx1 == null || vtx2 == null || !vtx1.nbrs.containsKey(vname2)) {
 		return;
 	}
-	v1.nbrs.remove(vname2);
-	v2.nbrs.remove(vname1);
+
+	vtx1.nbrs.remove(vname2);
+	vtx2.nbrs.remove(vname1);
 }
 public int numEdges() {
-	int count=0;
-	ArrayList<String> keys=new ArrayList<>(vtces.keySet());
-	for(String key:keys) {
-		Vertex vtx=vtces.get(key);
-		count=count+vtx.nbrs.size();
+
+	ArrayList<String> keys = new ArrayList<>(vtces.keySet());
+	int count = 0;
+
+	for (String key : keys) {
+
+		Vertex vtx = vtces.get(key);
+		count = count + vtx.nbrs.size();
 	}
-	return count/2;
-	
+
+	return count / 2;
 }
-public boolean containsEdge(String vname1,String vname2) {
-	Vertex v1=vtces.get(vname1);
-	Vertex v2=vtces.get(vname2);
-	if(v1==null||v2==null||!v1.nbrs.containsKey(vname2)) {
+public boolean containsEdge(String vname1, String vname2) {
+
+	Vertex vtx1 = vtces.get(vname1);
+	Vertex vtx2 = vtces.get(vname2);
+
+	if (vtx1 == null || vtx2 == null || !vtx1.nbrs.containsKey(vname2)) {
 		return false;
 	}
+
 	return true;
-	
-	
-	
 }
-public void addEdge(String vname1,String vname2,int cost) {
-	Vertex v1=vtces.get(vname1);
-	Vertex v2=vtces.get(vname2);
-	if(v1==null||v2==null||v1.nbrs.containsKey(vname2)) {
+public void addEdge(String vname1, String vname2, int cost) {
+
+	Vertex vtx1 = vtces.get(vname1); // 2k
+	Vertex vtx2 = vtces.get(vname2); // 4k
+
+	if (vtx1 == null || vtx2 == null || vtx1.nbrs.containsKey(vname2)) {
 		return;
 	}
-	v1.nbrs.put(vname2, cost);
-	v2.nbrs.put(vname1, cost);
-	
+
+	vtx1.nbrs.put(vname2, cost);
+	vtx2.nbrs.put(vname1, cost);
 }
 public void display() {
-	System.out.println("---------");
-	ArrayList<String> keys=new ArrayList<>(vtces.keySet());
-	for(String key:keys) {
-		Vertex vtx=vtces.get(key);
-		System.out.println(key+":"+vtx.nbrs);
+
+	System.out.println("------------------");
+	ArrayList<String> vnames = new ArrayList<>(vtces.keySet());
+
+	for (String vname : vnames) {
+
+		String str = vname + " => ";
+
+		Vertex vtx = vtces.get(vname);
+		str += vtx.nbrs;
+
+		System.out.println(str);
 	}
-	System.out.println("---------");
+	System.out.println("------------------");
+
 }
-public boolean hasPath(String vname1,String vname2,HashMap<String,Boolean> process) {
-	process.put(vname1, true);
-	if(containsEdge(vname1,vname2))
-	{
+public boolean hasPath(String vname1, String vname2, HashMap<String, Boolean> processed) {
+
+	// direct edge
+	if (containsEdge(vname1, vname2)) {
 		return true;
 	}
-	Vertex vtx=vtces.get(vname1);
-	ArrayList<String> nbrs=new ArrayList<>(vtces.keySet());
-	for(String nbr:nbrs) {
-		if(!process.containsKey(nbr)&&hasPath(nbr,vname2,process))
-		
-		return true;
-		
+
+	processed.put(vname1, true);
+
+	Vertex vtx = vtces.get(vname1);
+	ArrayList<String> nbrs = new ArrayList<>(vtx.nbrs.keySet());
+
+	for (String nbr : nbrs) {
+
+		if (!processed.containsKey(nbr) && hasPath(nbr, vname2, processed))
+			return true;
 	}
+
 	return false;
 }
+private class Pair {
+	String vname;
+	String psf;
+}
+public boolean bfs(String src, String dst) throws Exception {
+
+	HashMap<String, Boolean> processed = new HashMap<>();
+
+	LinkedListGeneric<Pair> queue = new LinkedListGeneric<>();
+
+	// create a new pair
+	Pair sp = new Pair();
+	sp.vname = src;
+	sp.psf = src;
+
+	// put the new pair in queue
+	queue.addLast(sp);
+
+	// while queue is not empty keep on doing the work
+	while (!queue.isEmpty()) {
+
+		// remove a pair from queue
+		Pair rp = queue.removeFirst();
+
+		if (processed.containsKey(rp.vname)) {
+			continue;
+		}
+
+		// processed put
+		processed.put(rp.vname, true);
+
+		// de
+		if (containsEdge(rp.vname, dst)) {
+			System.out.println(rp.psf + dst);
+			return true;
+		}
+
+		// nbrs
+		Vertex rpvtx = vtces.get(rp.vname);
+		ArrayList<String> nbrs = new ArrayList<>(rpvtx.nbrs.keySet());
+
+		// loop on nbrs
+		for (String nbr : nbrs) {
+
+			// process only unprocessed nbrs
+			if (!processed.containsKey(nbr)) {
+
+				// make a new pair of nbr and put in queue
+				Pair np = new Pair();
+				np.vname = nbr;
+				np.psf = rp.psf + nbr;
+
+				queue.addLast(np);
+			}
+		}
+
+	}
+
+	return false;
+
+}
+public boolean dfs(String src, String dst) throws Exception {
+
+	HashMap<String, Boolean> processed = new HashMap<>();
+
+	LinkedListGeneric<Pair> stack = new LinkedListGeneric<>();
+
+	// create a new pair
+	Pair sp = new Pair();
+	sp.vname = src;
+	sp.psf = src;
+
+	// put the new pair in queue
+	stack.addFirst(sp);
+
+	// while queue is not empty keep on doing the work
+	while (!stack.isEmpty()) {
+
+		// remove a pair from queue
+		Pair rp = stack.removeFirst();
+
+		if (processed.containsKey(rp.vname)) {
+			continue;
+		}
+
+		// processed put
+		processed.put(rp.vname, true);
+
+		// de
+		if (containsEdge(rp.vname, dst)) {
+			System.out.println(rp.psf + dst);
+			return true;
+		}
+
+		// nbrs
+		Vertex rpvtx = vtces.get(rp.vname);
+		ArrayList<String> nbrs = new ArrayList<>(rpvtx.nbrs.keySet());
+
+		// loop on nbrs
+		for (String nbr : nbrs) {
+
+			// process only unprocessed nbrs
+			if (!processed.containsKey(nbr)) {
+
+				// make a new pair of nbr and put in queue
+				Pair np = new Pair();
+				np.vname = nbr;
+				np.psf = rp.psf + nbr;
+
+				stack.addFirst(np);
+			}
+		}
+
+	}
+
+	return false;
+
+}
+public void bft() throws Exception {
+
+	HashMap<String, Boolean> processed = new HashMap<>();
+
+	LinkedListGeneric<Pair> queue = new LinkedListGeneric<>();
+
+	ArrayList<String> keys = new ArrayList<>(vtces.keySet());
+
+	// for every node repeat the process
+	for (String key : keys) {
+
+		if (processed.containsKey(key)) {
+			continue;
+		}
+
+		// create a new pair
+		Pair sp = new Pair();
+		sp.vname = key;
+		sp.psf = key;
+
+		// put the new pair in queue
+		queue.addLast(sp);
+
+		// while queue is not empty keep on doing the work
+		while (!queue.isEmpty()) {
+
+			// remove a pair from queue
+			Pair rp = queue.removeFirst();
+
+			if (processed.containsKey(rp.vname)) {
+				continue;
+			}
+
+			// processed put
+			processed.put(rp.vname, true);
+
+			// syso
+			System.out.println(rp.vname + " via " + rp.psf);
+
+			// nbrs
+			Vertex rpvtx = vtces.get(rp.vname);
+			ArrayList<String> nbrs = new ArrayList<>(rpvtx.nbrs.keySet());
+
+			// loop on nbrs
+			for (String nbr : nbrs) {
+
+				// process only unprocessed nbrs
+				if (!processed.containsKey(nbr)) {
+
+					// make a new pair of nbr and put in queue
+					Pair np = new Pair();
+					np.vname = nbr;
+					np.psf = rp.psf + nbr;
+
+					queue.addLast(np);
+				}
+			}
+
+		}
+
+	}
+
+}
+
+public void dft() throws Exception {
+
+	HashMap<String, Boolean> processed = new HashMap<>();
+
+	LinkedListGeneric<Pair> stack = new LinkedListGeneric<>();
+
+	ArrayList<String> keys = new ArrayList<>(vtces.keySet());
+
+	// for every node repeat the process
+	for (String key : keys) {
+
+		if (processed.containsKey(key)) {
+			continue;
+		}
+
+		// create a new pair
+		Pair sp = new Pair();
+		sp.vname = key;
+		sp.psf = key;
+
+		// put the new pair in queue
+		stack.addFirst(sp);
+
+		// while queue is not empty keep on doing the work
+		while (!stack.isEmpty()) {
+
+			// remove a pair from queue
+			Pair rp = stack.removeFirst();
+
+			if (processed.containsKey(rp.vname)) {
+				continue;
+			}
+
+			// processed put
+			processed.put(rp.vname, true);
+
+			// syso
+			System.out.println(rp.vname + " via " + rp.psf);
+
+			// nbrs
+			Vertex rpvtx = vtces.get(rp.vname);
+			ArrayList<String> nbrs = new ArrayList<>(rpvtx.nbrs.keySet());
+
+			// loop on nbrs
+			for (String nbr : nbrs) {
+
+				// process only unprocessed nbrs
+				if (!processed.containsKey(nbr)) {
+
+					// make a new pair of nbr and put in queue
+					Pair np = new Pair();
+					np.vname = nbr;
+					np.psf = rp.psf + nbr;
+
+					stack.addFirst(np);
+				}
+			}
+
+		}
+
+	}
+
+}
+public boolean isCyclic() throws Exception {
+
+	HashMap<String, Boolean> processed = new HashMap<>();
+
+	LinkedListGeneric<Pair> queue = new LinkedListGeneric<>();
+
+	ArrayList<String> keys = new ArrayList<>(vtces.keySet());
+
+	// for every node repeat the process
+	for (String key : keys) {
+
+		if (processed.containsKey(key)) {
+			continue;
+		}
+
+		// create a new pair
+		Pair sp = new Pair();
+		sp.vname = key;
+		sp.psf = key;
+
+		// put the new pair in queue
+		queue.addLast(sp);
+
+		// while queue is not empty keep on doing the work
+		while (!queue.isEmpty()) {
+
+			// remove a pair from queue
+			Pair rp = queue.removeFirst();
+
+			if (processed.containsKey(rp.vname)) {
+				return true;
+			}
+
+			// processed put
+			processed.put(rp.vname, true);
+
+			// nbrs
+			Vertex rpvtx = vtces.get(rp.vname);
+			ArrayList<String> nbrs = new ArrayList<>(rpvtx.nbrs.keySet());
+
+			// loop on nbrs
+			for (String nbr : nbrs) {
+
+				// process only unprocessed nbrs
+				if (!processed.containsKey(nbr)) {
+
+					// make a new pair of nbr and put in queue
+					Pair np = new Pair();
+					np.vname = nbr;
+					np.psf = rp.psf + nbr;
+
+					queue.addLast(np);
+				}
+			}
+
+		}
+
+	}
+
+	return false;
+
+}
+
+public boolean isConnected() throws Exception {
+
+	int flag = 0;
+
+	HashMap<String, Boolean> processed = new HashMap<>();
+
+	LinkedListGeneric<Pair> queue = new LinkedListGeneric<>();
+
+	ArrayList<String> keys = new ArrayList<>(vtces.keySet());
+
+	// for every node repeat the process
+	for (String key : keys) {
+
+		if (processed.containsKey(key)) {
+			continue;
+		}
+
+		flag++;
+
+		// create a new pair
+		Pair sp = new Pair();
+		sp.vname = key;
+		sp.psf = key;
+
+		// put the new pair in queue
+		queue.addLast(sp);
+
+		// while queue is not empty keep on doing the work
+		while (!queue.isEmpty()) {
+
+			// remove a pair from queue
+			Pair rp = queue.removeFirst();
+
+			if (processed.containsKey(rp.vname)) {
+				continue;
+			}
+
+			// processed put
+			processed.put(rp.vname, true);
+
+			// nbrs
+			Vertex rpvtx = vtces.get(rp.vname);
+			ArrayList<String> nbrs = new ArrayList<>(rpvtx.nbrs.keySet());
+
+			// loop on nbrs
+			for (String nbr : nbrs) {
+
+				// process only unprocessed nbrs
+				if (!processed.containsKey(nbr)) {
+
+					// make a new pair of nbr and put in queue
+					Pair np = new Pair();
+					np.vname = nbr;
+					np.psf = rp.psf + nbr;
+
+					queue.addLast(np);
+				}
+			}
+
+		}
+
+	}
+
+	if (flag >= 2) {
+		return false;
+	} else {
+		return true;
+	}
+
+}
+public boolean isTree() throws Exception {
+	return !isCyclic() && isConnected();
+}
+public ArrayList<ArrayList<String>> getCC() throws Exception {
+
+	ArrayList<ArrayList<String>> ans = new ArrayList<>();
+
+	HashMap<String, Boolean> processed = new HashMap<>();
+
+	LinkedListGeneric<Pair> queue = new LinkedListGeneric<>();
+
+	ArrayList<String> keys = new ArrayList<>(vtces.keySet());
+
+	// for every node repeat the process
+	for (String key : keys) {
+
+		if (processed.containsKey(key)) {
+			continue;
+		}
+
+		// for new component create a new arrayList
+		ArrayList<String> subans = new ArrayList<>();
+
+		// create a new pair
+		Pair sp = new Pair();
+		sp.vname = key;
+		sp.psf = key;
+
+		// put the new pair in queue
+		queue.addLast(sp);
+
+		// while queue is not empty keep on doing the work
+		while (!queue.isEmpty()) {
+
+			// remove a pair from queue
+			Pair rp = queue.removeFirst();
+
+			if (processed.containsKey(rp.vname)) {
+				continue;
+			}
+
+			// processed put
+			processed.put(rp.vname, true);
+
+			// put in subans arraylist
+			subans.add(rp.vname);
+
+			// nbrs
+			Vertex rpvtx = vtces.get(rp.vname);
+			ArrayList<String> nbrs = new ArrayList<>(rpvtx.nbrs.keySet());
+
+			// loop on nbrs
+			for (String nbr : nbrs) {
+
+				// process only unprocessed nbrs
+				if (!processed.containsKey(nbr)) {
+
+					// make a new pair of nbr and put in queue
+					Pair np = new Pair();
+					np.vname = nbr;
+					np.psf = rp.psf + nbr;
+
+					queue.addLast(np);
+				}
+			}
+
+		}
+
+		// put subans in final ans
+		ans.add(subans);
+
+	}
+
+	return ans;
+
+}
+
+
 }
 
